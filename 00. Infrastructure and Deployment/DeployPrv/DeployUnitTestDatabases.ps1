e303674c233d63c94b562464aa38d28d7fa54cd2
Pieter Laurens Baljon <pieterlaurens@gmail.com>
2018-03-26 15:17:10 +0200

    Added quotes around the server name, and pointed the deploy script to the right sqlpackage.exe location.

--- 00. Infrastructure and Deployment/DeployPrv/DeployUnitTestDatabases.ps1
+++ 00. Infrastructure and Deployment/DeployPrv/DeployUnitTestDatabases.ps1
@@ -11,14 +11,14 @@ $BuildConfiguration = $args[1] #Release or Debug; only applies to project DB
 $BuildVersion = $args[2]
 
 #$sqlPackage = "C:\Program Files (x86)\Microsoft SQL Server\130\DAC\bin\sqlpackage.exe"
-$sqlPackage = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe"
+$sqlPackage = "C:\Program Files (x86)\Microsoft Visual Studio\2017\SQL\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\140\sqlpackage.exe"
 
 # 1b. publish Data Handler
 Write-Output "==================== Step 1: Publish Data Handlers DB"
 $dthDacPacSource = "01. Main\04. Storage schema\02. SQL Server\CentralViewsDB\CentralViewsDB\bin\Release\CentralViewsDB.dacpac"
 $dthProfile = "01. Main\04. Storage schema\02. SQL Server\CentralViewsDB\CentralViewsDB\CentralViewsDB.dev.publish.xml"
 Invoke-Sqlcmd -Query "IF(DB_ID('prv_tst_dth') is not null) BEGIN alter database prv_tst_dth set single_user with rollback immediate; DROP DATABASE prv_tst_dth END" -ServerInstance $TargetSqlServer
-& $sqlPackage /Action:Publish /tsn:$TargetSqlServer /tdn:prv_tst_dth /sf:$dthDacPacSource /profile:$dthProfile  /v:BuildVersion=$BuildVersion
+& $sqlPackage /Action:Publish /tsn:"$TargetSqlServer" /tdn:prv_tst_dth /sf:$dthDacPacSource /profile:$dthProfile  /v:BuildVersion=$BuildVersion
 
 # 5. Copy ProjectDb component dacpacs to target server
 Write-Output "==================== Step 2: Publish project datamart"
@@ -30,8 +30,8 @@ foreach($ModuleName in $ProjectDbModules){
     $moduleDacPacSource = "01. Main\97. ProjectDatamart\$ModuleName\bin\$BuildConfiguration\ProjectDb$ModuleName.dacpac"
     $moduleProfile = "01. Main\97. ProjectDatamart\$ModuleName\bin\$BuildConfiguration\ProjectDb$ModuleName.publish.xml"
 	if($ModuleName -eq "Bacmap"){
-		& $sqlPackage /Action:Publish /tsn:$TargetSqlServer /tdn:prv_prj_unittest /sf:$moduleDacPacSource /profile:$moduleProfile /v:DataHandlerDb=prv_tst_dth /v:ProjectDbQuestionnaire=prv_prj_unittest
+		& $sqlPackage /Action:Publish /tsn:"$TargetSqlServer" /tdn:prv_prj_unittest /sf:$moduleDacPacSource /profile:$moduleProfile /v:DataHandlerDb=prv_tst_dth /v:ProjectDbQuestionnaire=prv_prj_unittest
 	} else {
-		& $sqlPackage /Action:Publish /tsn:$TargetSqlServer /tdn:prv_prj_unittest /sf:$moduleDacPacSource /profile:$moduleProfile
+		& $sqlPackage /Action:Publish /tsn:"$TargetSqlServer" /tdn:prv_prj_unittest /sf:$moduleDacPacSource /profile:$moduleProfile
 	}
 }
